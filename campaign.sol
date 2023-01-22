f25ecf1537d0da31751dc8446e0577d5760b0e62
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-01-22 14:44:04 -0800

    Update campaign.sol

--- campaign.sol
+++ campaign.sol
@@ -1,76 +1,86 @@
-// SPDX-License-Identifier: GPL-3.0
+//  SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
-
+pragma solidity ^0.8.2;
+ 
 contract CampaignFactory {
-    address[] public deployedCampaigns;
-
+    Campaign[] public deployedCampaigns;
+    
     function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
-
-    function getDeployedCampaigns() public view returns (address[]) {
+    
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
         return deployedCampaigns;
-
     }
 }
-
+ 
 contract Campaign {
     struct Request {
         string description;
         uint value;
-        address recipient;
+        address payable recipient;
         bool complete;
+        mapping (address => bool) approvals;
         uint approvalCount;
-        mapping(address => bool) approvals;
     }
-
-    Request[] public requests;
+    
     address public manager;
     uint public minimumContribution;
-    // address[] public approvers;
     mapping(address => bool) public approvers;
     uint public approversCount;
-
-
-    modifier restricted() {
+    
+    uint numRequests;
+    mapping (uint => Request) requests;
+    
+    
+    modifier restrictedToManager() {
         require(msg.sender == manager);
         _;
     }
-    constructor(uint minimum, address creator) public {
+    
+    constructor(uint minimum, address creator) {
         manager = creator;
         minimumContribution = minimum;
     }
-
+    
     function contribute() public payable {
-        require(msg.value>minimumContribution);
-        // approvers.push(msg.sender);
+        require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
-        approversCount++;
+        approversCount ++;
     }
-
+    
+    function createRequest(string calldata description, uint value, address payable recipient) public restrictedToManager {
+        // get last index of requests from storage
+       Request storage newRequest = requests[numRequests];
+       // increase requests counter
+       numRequests ++;
+       // add information about new request
+       newRequest.description = description;
+       newRequest.value = value;
+       newRequest.recipient = recipient;
+       newRequest.approvalCount = 0;
+    }
+    
     function approveRequest(uint index) public {
-        // change directly in memory address
+        // get request at provided index from storage
         Request storage request = requests[index];
+        // sender needs to have contributed to Campaign
         require(approvers[msg.sender]);
+        // sender must not have voted yet
         require(!request.approvals[msg.sender]);
-
+        
+        // add sender to addresses who have voted
         request.approvals[msg.sender] = true;
-        request.approvalCount++;
+        // increment approval count
+        request.approvalCount ++;
     }
-
-    function finalizeRequest(uint index) public restricted{
+    
+    function finalizeRequest(uint index) public restrictedToManager {
         Request storage request = requests[index];
-
-        // more than half of the population to finalize the request
-        require(request.approvalCount>approversCount/2);
         require(!request.complete);
-
+        require(request.approvalCount > (approversCount / 2));
         request.recipient.transfer(request.value);
         request.complete = true;
     }
-
-
-
 }
