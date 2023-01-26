698988fa47c9a02acf3a88590dfffa7b09124ad9
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-01-26 13:20:12 -0800

    Update campaign - Copy.sol

--- campaign - Copy.sol
+++ campaign - Copy.sol
@@ -20,22 +20,30 @@ contract Products {
     struct Product {
         string name;
         string description;
+        uint amt;
         uint price;
         // address payable escrowContracts;
-        address payable seller;
-        mapping (address => bool) confirmations;
-        mapping (address => string) buyer_id;
+        address payable deposit_fund;
+        // mapping (address => bool) confirmations;
+        mapping (address => bool) buyer_confirmations;
         // float rating;
         // mapping (float => string) rating_review;
-        uint amt;
+
     }
 
+    struct Seller {
+        string name;
+        Product[] productid;
+        uint rating;
+    }
+
+    // Product[] public products;
     address public seller;
     uint public numProducts;
     uint public minimumContribution;
 
     mapping(uint => Product) public products;
-    mapping(address => bool) public sellers;
+    mapping(address => bool) public buyers;
 
     constructor(uint minimum, address creator) {
         seller = creator;
@@ -45,7 +53,7 @@ contract Products {
     function createProduct(
         string calldata description, uint price,
         uint amt
-        , address payable seller
+        , address payable deposit_fund
     ) public restrictedToSeller {
         require(address(msg.sender).balance > price*2, "The msg.sender is not payable");
         //  create new product
@@ -55,28 +63,38 @@ contract Products {
        // add information about new request
        newProduct.description = description;
        newProduct.price = price;
-       newProduct.seller = seller;
+       newProduct.deposit_fund = deposit_fund;
        newProduct.amt = amt;
     }
 
+    function approveReceipt(uint index) public {
+        Product storage request = products[index];
+        // get request at provided index from storage
+        require(buyers[msg.sender]);
+        // sender must not have voted yet ?????
+        require(!request.buyer_confirmations[msg.sender]);
+    }
+
+
+
     function purchase(address escrow) public payable {
         require(address(msg.sender).balance >= products[numProducts].price*2); // ensure the seller has enough ether to fulfill the purchase
-        sellers[msg.sender] = true;
+        buyers[msg.sender] = true;
     }
     
-    function approveReceipt(uint index) public {
-        // get request at provided index from storage
-        Product storage request = products[numProducts];
-        // sender needs to have contributed to Campaign
-        require(sellers[msg.sender]);
-        // sender must not have voted yet
-        require(!request.confirmations[msg.sender]);
+    // function approveReceipt(uint index) public {
+    //     // get request at provided index from storage
+    //     Product storage request = products[numProducts];
+    //     // sender needs to have contributed to Campaign
+    //     require(sellers[msg.sender]);
+    //     // sender must not have voted yet
+    //     require(!request.buyer_confirmations[msg.sender]);
         
-        // add sender to addresses who have voted
-        request.confirmations[msg.sender] = true;
-        // increment approval count
-        request.numProducts --;
-    }
+    //     // add sender to addresses who have voted
+    //     request.confirmations[msg.sender] = true;
+    //     // increment approval count
+    //     request.numProducts --;
+    // }
 
 modifier restrictedToSeller() {
         require(msg.sender == seller);
