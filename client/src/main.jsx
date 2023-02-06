786d7505b3528b8dab18f63832a6e3c0ce2a6b17
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-02-06 15:16:59 -0800

    import react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 
-import App from './App'
+
 import './index.css'
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
