0c4bd50abb7b270793618ef5de3dcba1a718b4a4
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-02-06 00:04:11 -0800

    Create link pages components

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,6 +1,10 @@
+import {Route, Routes} from 'react-router-dom'
 const Services = () => {
     return (
         <h1>Services</h1>
+        // <Routes>
+        //     <Route path = "/" element = {<Home/>} />
+        // </Routes>
     );
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export {default as Home} from './Home';
+export {default as CreateProduct} from './CreateProduct';
+export {default as Transactions} from './Transactions';
