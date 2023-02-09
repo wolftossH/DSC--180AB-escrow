f9762de542f31a989e5856e8ed34c43edfe37dda
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-02-08 19:22:16 -0800

    Update index.jsx

--- new_client/src/context/index.jsx
+++ new_client/src/context/index.jsx
@@ -1,7 +1,37 @@
 import React, { useContext, createContext } from 'react';
 
-// import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
-// import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
 const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    // const { contract } = useContract(
+    //     '0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9'
+    // );
+    const { mutateAsync: createProduct } = useContractWrite(contract, 'createCampaign');
+  
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishProduct = async (form) => {
+        try {
+          const data = await createProduct([
+            address, // owner
+            form.title, // title
+            form.description, // description
+            form.target,
+            new Date(form.deadline).getTime(), // deadline,
+            form.deposit,
+            form.image
+          ])
+    
+          console.log("contract call success", data)
+        } catch (error) {
+          console.log("contract call failure", error)
+        }
+      }
+}
+
+export const useStateContext = () => useContext(StateContext);
