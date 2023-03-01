76739af52a0f68a8b7b86bef4c52a14ff89435a9
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-03-01 12:53:37 -0800

    Moved files

--- new_client/src/components/index.js
+++ new_client/src/components/index.js
@@ -2,7 +2,7 @@ export { default as Loader } from "./Loader";
 export { default as Navbar } from "./Navbar";
 export { default as Navbar_new } from "./Navbar_new";
 
-export { default as Welcome } from "./Welcome";
+// export { default as Welcome } from "./Welcome";
 export { default as Services } from "./Services";
 export { default as CustomButton } from './CustomButton';
 export { default as FormField } from './FormField';
--- new_client/src/components_testing/Welcome copy.jsx
+++ new_client/src/components_testing/Welcome copy.jsx
@@ -6,7 +6,7 @@ import { SiEthereum } from "react-icons/si";
 
 // import { TransactionContext } from "../context/TransactionContext";
 // import { shortenAddress } from "../utils/shortenAddress";
-import { Loader } from ".";
+import { Loader } from "../components";
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
