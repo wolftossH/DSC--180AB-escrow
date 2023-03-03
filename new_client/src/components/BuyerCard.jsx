1886b2d3ba10a4ec4b897c9d90846825119401c5
wolftossH <57511507+wolftossH@users.noreply.github.com>
2023-03-03 14:58:12 -0800

    Update BuyerCard.jsx

--- new_client/src/components/BuyerCard.jsx
+++ new_client/src/components/BuyerCard.jsx
@@ -5,30 +5,31 @@ import useFetch from "../hooks/gifGen";
 
 const BuyerCard = ({buyers, title, }) => {
 //   const gifUrl = useFetch({ title });
+console.log(buyers)
 
   return (
     
     
-<div class="w-full max-w-2xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-5 dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex items-center justify-between mb-4">
-        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">{title}</h5>
+<div className="w-full max-w-2xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-5 dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">{title}</h5>
    </div>
 
-   <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+   <div className="flow-root">
+        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
             {buyers.length > 0 ? buyers.map((item, index) => (
-                <li class="py-3 sm:py-4">
-                    <div class="flex items-center space-x-4">
-                        <div class="flex-shrink-0">
-                            <div class="w-8 h-8 rounded-full ">
+                <li key={index} className="py-3 sm:py-4">
+                    <div className="flex items-center space-x-4">
+                        <div className="flex-shrink-0">
+                            <div className="w-8 h-8 rounded-full ">
                                 {index+1}
                             </div>
                         </div>
-                        <div class="flex-1 min-w-0">
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        <div className="flex-1 min-w-0">
+                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
                                 {item.buyer_address}
                             </p>
-                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                                 {item.delivery_address}
                             </p>
                         </div>
